a8af1627a6074a7fe3526e16d12020898b22aeea
Jamison Cozart and Kristina Hengster Tintor <kristina@>
2020-02-04 14:56:45 -0800

    pass test for addRow() method

--- src/sudoku.js
+++ src/sudoku.js
@@ -7,5 +7,5 @@ Board.prototype.addRow = function(row) {
 }
 
 export function Row() {
-//   this.values = ["", "", "", "", "", "", "", "", ""];
+  this.values = ["", "", "", "", "", "", "", "", ""];
 }
